5af057ccecee2ddd8047abcb0ca001ffc1140555
Geoffrey Okumu <geoffocum@gmail.com>
2022-07-27 05:36:12 +0000

    v1 release

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.hbs", "./**/*.hbs"],
+    theme: {
+        extend: {
+            keyframes: {
+                zoom: {
+                    "0%, 100": { transform: "scale(1)" },
+                    "50%": { transform: "scale(1.1)" },
+                },
+            },
+            animation: {
+                zoom: "zoom 20s ease-in infinite",
+            },
+        },
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+        fontFamily: {
+            sans: ["IBM Plex Sans", "sans-serif"],
+            serif: ["IBM Plex Serif", "sans-serif"],
+        },
+    },
+    plugins: [],
+};
