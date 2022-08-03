a620329b374289c0e673bfd440389635f4605e46
Geoffrey Okumu <geoffocum@gmail.com>
2022-08-03 13:24:12 +0000

    v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.hbs", "./**/*.hbs"],
-    theme: {
-        extend: {
-            keyframes: {
-                zoom: {
-                    "0%, 100": { transform: "scale(1)" },
-                    "50%": { transform: "scale(1.1)" },
-                },
-            },
-            animation: {
-                zoom: "zoom 20s ease-in infinite",
-            },
+  content: ["./*.hbs", "./**/*.hbs"],
+  theme: {
+    extend: {
+      keyframes: {
+        zoom: {
+          "0%, 100": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
         },
-        container: {
-            center: true,
-            padding: "1rem",
-        },
-        fontFamily: {
-            sans: ["IBM Plex Sans", "sans-serif"],
-            serif: ["IBM Plex Serif", "sans-serif"],
-        },
+      },
+      animation: {
+        zoom: "zoom 20s ease-in infinite",
+      },
     },
-    plugins: [],
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    fontFamily: {
+      sans: ["IBM Plex Sans", "sans-serif"],
+      serif: ["Playfair Display", "sans-serif"],
+    },
+  },
+  plugins: [],
 };
